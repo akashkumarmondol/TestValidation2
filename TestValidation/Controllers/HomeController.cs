bfa5f954dc6e06377bb18f50a126efbd133d3d4f
Akash Kumar Mondol <akashkumarmondol123@gmail.com>
2023-10-28 11:40:35 +0600

    Initial Push

---
+++ TestValidation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TestValidation/Scripts/GoogleCaptcha.js
@@ -0,0 +1,14 @@
+﻿function submitForm() {
+    // Get the response from the reCAPTCHA widget
+    var recaptchaResponse = grecaptcha.getResponse();
+
+    // Check if the reCAPTCHA widget has been completed
+    if (recaptchaResponse.length == 0) {
+        // The reCAPTCHA widget has not been completed, show an alert
+        alert('Please complete the reCAPTCHA to submit the form.');
+        return false;
+    } else {
+        // The reCAPTCHA widget has been completed, submit the form
+        return true;
+    }
+}
